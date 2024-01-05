1a4f0ec7b7ce5ed0b90d8b37b8b98da1274a21b2
RuchikaJamode <ruchikasjamode@gmail.com>
2024-01-05 19:20:48 +0530

    welcome

---
+++ main.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.generic;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer[] intArray = {1, 2, 3, 4, 5 }; 
+		Double[] doubleArray = { 1.1, 2.2, 3.3, 4.7 };
+
+		Character[] charArray = { 'H', 'E', 'L', 'L', '0' };
+
+		print_Array.toPrint(intArray);
+		print_Array.toPrint(doubleArray);
+		print_Array.toPrint(charArray);
+	}
+
+}
---
+++ print_Array.java
@@ -0,0 +1,34 @@
+package com.bridgelabz.generic;
+
+public class print_Array {
+
+	public static void toPrint(Integer[] inputArray) {
+		for(int element: inputArray) { System.out.printf("%s", element); }
+
+		System.out.println();
+
+		}
+
+		public static void toPrint (Double[] inputArray) { 
+			for(double element: inputArray) {
+				System.out.printf("%s are ", element);
+				}
+
+		System.out.println();
+
+		}
+
+		public static void toPrint (Character [] inputArray) { 
+			for(char element: inputArray) { 
+				System.out.printf("%s", element);
+
+		System.out.println();
+
+		}
+
+		
+		
+
+	}
+
+}
